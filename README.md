12c949a641a8897d3a538db0c0966de5848bc8d1
TheMrGhostman <ghostmanthe@gmail.com>
2023-09-04 17:17:28 +0200

    preprocess

--- README.md
+++ README.md
@@ -4,3 +4,18 @@ Repository in development!
 It is focused on comparision of approaches to classify tree-structured data. It lists for now simple KNN with various tree metrices and hmill classifier.
 
 (GHMill package from dependencies is now avaliable online for now)
+
+datasets| classes |
+---|---|
+ "Mutagenesis"| 2 |
+ "chess"| 3 |
+ "citeseer"| 6 |
+ "cora"| 7 |
+ "craft_beer"| 51 |
+ "ftp"| 3 |
+ "genes"| 15 |
+ "hepatitis"| 2 |
+ "pubmed_diabetes"| 3 |
+ "uw_cse"| 4 |
+ "webkp"| 5 |
+ "world"| 7 |
--- src/dataloading.jl
+++ src/dataloading.jl
@@ -31,3 +31,27 @@ function _to_mill(x)
     # access ngram string as data.S
     return extractor.(x);  
 end
+
+
+function preprocess(X, y; ratios=(0.6,0.2,0.2), procedure=:clf, seed=666)
+    @assert sum(ratios) == 1 && length(ratios) == 3
+    if procedure==:clf
+        Random.seed!(seed)
+        (X_tr, y_tr), rest = MLUtils.splitobs((X,y); at=ratios[1], shuffle=true)
+        (X_val, y_val), (X_tst, y_tst) = MLUtils.splitobs(rest; at=ratios[2], shuffle=false)
+        Random.seed!()
+    elseif procedure==:ad
+        Random.seed!(seed)
+        # first split clean train/val/test
+        (X_tr, y_tr), rest = MLUtils.splitobs((X[y.==0], y[y.==0]); at=ratios[1], shuffle=true)
+        (X_cval, y_cval), (X_ctst, y_ctst) = MLUtils.splitobs(rest; at=ratios[2], shuffle=false)
+        # secondly split anomalous to val/test because they are not needed in train 
+        (X_aval, y_aval), (X_atst, y_atst) = MLUtils.splitobs((X[y.==1],y[y.==1]); at=0.5, shuffle=true)
+        Random.seed!()
+        (X_val, y_val) = (vcat(X_cval, X_aval), vcat(y_cval, y_aval))
+        (X_tst, y_tst) = (vcat(X_ctst, X_atst), vcat(y_ctst, y_atst)) 
+    else
+        @error("Unknown preprocessing procedure. Options are :clf for classification and :ad for anomaly detection")
+    end
+    return (X_tr, y_tr), (X_val, y_val), (X_tst, y_tst)
+end
