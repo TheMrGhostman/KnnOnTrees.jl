10d1219e0f98123251c1a21b1e4e6e0044ef1df8
TheMrGhostman <34610021+TheMrGhostman@users.noreply.github.com>
2024-02-14 20:20:02 +0100

    TUDataset adde into loading function

--- src/dataloading.jl
+++ src/dataloading.jl
@@ -6,19 +6,25 @@ function get_list_of_datasets()
 end
 
 
-function load_dataset(name; to_mill=true)
-    @assert name in get_list_of_datasets()
-
-    name = lowercase(name)
-    if  name == "mutagenesis"
+function load_dataset(name; to_mill::Bool=true, to_pad_leafs::Bool=false, depth::Int=3)
+    #@assert name in get_list_of_datasets()
+    #name = lowercase(name)
+    if  name in ["mutagenesis", "Mutagenesis"]
         data = MLDatasets.Mutagenesis(split=:all);
         X = data.features
         y = data.targets .+ 1
-    else 
+    elseif name in get_list_of_datasets()
         data_string = read(datadir("relational/$(name).json"), String);
         data = JSON3.read(data_string);
         X = data.x
         y = data.y
+    else
+        data = MLDatasets.TUDataset(name)
+        n_class = get_n_unique_nodes(data) |> length
+        transform = (to_mill) ? x->graph2hmill(x, n_class, depth; pad=to_pad_leafs) : identity
+        X = [transform(data[i].graphs) for i in only(axes(data))]
+        y = data.graph_data.targets
+        to_mill = false # no need to du _to_mill anymore
     end
     X = (to_mill) ? _to_mill(X) : X
     return X, y
